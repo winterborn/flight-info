edc67fcebebfe4d0fe40bc7177911cc7660344e5
Philip White <phwhite89@gmail.com>
2023-07-20 20:39:47 +0100

    experimenting with loading message

--- frontend/src/components/FlightList.tsx
+++ frontend/src/components/FlightList.tsx
@@ -5,12 +5,14 @@ import { Flight, FlightFilter } from "../types/types";
 
 const FlightList: React.FC<FlightFilter> = ({ filter, searchTerm }) => {
   const [flights, setFlights] = useState<Flight[]>([]);
+  const [loading, setLoading] = useState<boolean>(true);
   const [error, setError] = useState<string | null>(null);
 
   // console.log("FlightList - searchTerm:", searchTerm);
 
   useEffect(() => {
     const fetchFlights = async () => {
+      setLoading(true); // Set loading to true when fetching starts
       try {
         let baseUrl = "http://localhost:4000/api/flights";
         let path = "/";
@@ -37,6 +39,8 @@ const FlightList: React.FC<FlightFilter> = ({ filter, searchTerm }) => {
         setError(
           `An error occurred while fetching flights. Please try again later.`
         );
+      } finally {
+        setLoading(false); // Set loading to false when fetching is done (regardless of success or error)
       }
     };
 
@@ -54,6 +58,10 @@ const FlightList: React.FC<FlightFilter> = ({ filter, searchTerm }) => {
     </p>
   );
 
+  const isLoadingMessage = (
+    <p className="is-loading-message">Loading flight data...</p>
+  );
+
   const renderErrorMessage = () => {
     if (error) {
       return <p>{error}</p>;
@@ -69,24 +77,27 @@ const FlightList: React.FC<FlightFilter> = ({ filter, searchTerm }) => {
           : filter.charAt(0).toUpperCase() + filter.slice(1)}
       </h2>
       {error && <div className="error-message">{renderErrorMessage()}</div>}
+      {loading && loading && isLoadingMessage}
       {!error && filteredFlights.length === 0 && noFlightsMessage}
-      {!error && filteredFlights.map((flight) => (
-        <div className="flight-details" key={flight.FlightNo}>
-          <img src={flight.Image} alt="" />
-          <h2>
-            {flight.FlightNo} {flight.Airline}
-          </h2>
-          <p>
-            {flight.Date} {flight.Time}
-          </p>
-          <p>{flight.PortOfCallA}</p>
-          <p>{flight.Status}</p>
+      {!error &&
+        !loading &&
+        filteredFlights.map((flight) => (
+          <div className="flight-details" key={flight.FlightNo}>
+            <img src={flight.Image} alt="" />
+            <h2>
+              {flight.FlightNo} {flight.Airline}
+            </h2>
+            <p>
+              {flight.Date} {flight.Time}
+            </p>
+            <p>{flight.PortOfCallA}</p>
+            <p>{flight.Status}</p>
 
-          <Link to={`/flight/${flight.FlightNo}`} state={{ filter }}>
-            <h3 className="details-button">Details</h3>
-          </Link>
-        </div>
-      ))}
+            <Link to={`/flight/${flight.FlightNo}`} state={{ filter }}>
+              <h3 className="details-button">Details</h3>
+            </Link>
+          </div>
+        ))}
     </div>
   );
 };
